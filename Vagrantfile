d4a131ef7d96ce4a8ff2bd4641cfe25a8b8a9b5d
Victor Gan <victorgan@gmail.com>
2015-02-11 11:28:55 -0800

    cleaned up

--- Vagrantfile
+++ Vagrantfile
@@ -5,18 +5,15 @@
 # configures the configuration version (we support older styles for
 # backwards compatibility). Please don't change it unless you know what
 # you're doing.
-Vagrant.configure(2) do |config|
-  # The most common configuration options are documented and commented below.
-  # For a complete reference, please see the online documentation at
-  # https://docs.vagrantup.com.
+VAGRANTFILE_API_VERSION = "2"
+Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
+  # For a complete reference, see  https://docs.vagrantup.com.
 
-  # Every Vagrant development environment requires a box. You can search for
   # boxes at https://atlas.hashicorp.com/search.
   config.vm.box = "ubuntu/trusty64"
+  config.vm.box_url = "https://atlas.hashicorp.com/ubuntu/boxes/trusty64"
 
-  # Enable provisioning with a shell script. Additional provisioners such as
-  # Puppet, Chef, Ansible, Salt, and Docker are also available. Please see the
-  # documentation for more information about their specific syntax and use.
+  # Enable provisioning with a shell script. 
   config.vm.provision :shell, path: "shellcommands.sh"
   
 end
--- shellcommands.sh
+++ shellcommands.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 echo "Starting shellcommands.sh vagrant provisioning script"
 
-echo "Updated apt-get"
+echo "Updating apt-get"
 apt-get update
 
+# ------------------------------
+# Git, Vim, Bash
+# ------------------------------
 # -y  Assume Yes to all queries and do not prompt
 # outputs stream to /dev/null 
 echo "Installing Git"
@@ -15,3 +18,32 @@ echo "source $HOME/.vim/vimrc" > $HOME/.vimrc
 
 echo "Customizing Bash Settings"
 echo "export TERM=xterm-256color" > $HOME/.bashrc
+
+# ------------------------------
+# Python
+# ------------------------------
+echo "Installing full SciPy Stack" # http://www.scipy.org/install.html
+sudo apt-get install -y python-numpy python-scipy python-matplotlib ipython ipython-notebook python-pandas python-sympy python-nose
+
+# echo "Install PIP"
+# wget https://bootstrap.pypa.io/get-pip.py -O - | python
+# pip install -U pip # upgrade pip
+
+# echo "Install Pillow (fork of PIL)"
+# pip install Pillow
+
+
+# ------------------------------
+# OpenCV
+# ------------------------------
+# Untested. See https://github.com/razius/ppy-opencv-vagrant/blob/master/bootstrap.sh
+# echo "Installing OpenCV"
+# pushd /tmp
+#     git clone https://github.com/jayrambhia/Install-OpenCV.git
+#     pushd Install-OpenCV/Ubuntu/
+#         ./opencv_latest.sh
+#     popd
+# popd
+
+# echo "Installing Pandoc"
+# apt-get install pandoc -y
