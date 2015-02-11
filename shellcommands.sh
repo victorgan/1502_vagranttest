2229f0ce8ce26d26d22c6ee483163939777657e7
Victor Gan <victorgan@gmail.com>
2015-02-10 22:37:26 -0800

    better bash provisioning

--- shellcommands.sh
+++ shellcommands.sh
@@ -8,3 +8,10 @@ apt-get update
 # outputs stream to /dev/null 
 echo "Installing Git"
 apt-get install git -y > /dev/null
+
+echo "Customizing Vim Settings"
+git clone git://github.com/victorgan/.vim.git $HOME/.vim
+echo "source $HOME/.vim/vimrc" > $HOME/.vimrc
+
+echo "Customizing Bash Settings"
+echo "export TERM=xterm-256color" > $HOME/.bashrc
